ff133ffa17e28e2a292dc18f93b0d985a97048b5
Calvin Koepke <2815568+cjkoepke@users.noreply.github.com>
2021-12-16 00:07:02 -0700

    fix: official policy id

--- src/lib/helpers/env.ts
+++ src/lib/helpers/env.ts
@@ -48,7 +48,7 @@ export const usePolicyID = (): string => {
   const isProduction = useIsProduction();
 
   useEffect(() => {
-    setPolicyID(isProduction ? 'd5df2ddadd04b98215f7c3ea94fd9ab8194968f94d9d32377fd26a7c' : '8d18d786e92776c824607fd8e193ec535c79dc61ea2405ddf3b09fe3');
+    setPolicyID(isProduction ? 'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a' : '8d18d786e92776c824607fd8e193ec535c79dc61ea2405ddf3b09fe3');
   }, [isProduction]);
 
   return policyID;
