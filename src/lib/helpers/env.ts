3bb9c3c58c3f377e663178b92e2b4c4ea9d36a30
Calvin Koepke <2815568+cjkoepke@users.noreply.github.com>
2021-12-16 14:05:41 -0700

    fix: useeffect

--- src/lib/helpers/env.ts
+++ src/lib/helpers/env.ts
@@ -1,8 +1,8 @@
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useSSR = () => {
   const [isSSR, setIsSSR] = useState(true);
-  useMemo(() => {
+  useEffect(() => {
     setIsSSR(typeof window === undefined);
   }, []);
 
@@ -12,7 +12,7 @@ export const useSSR = () => {
 export const useIsProduction = (): boolean => {
   const [isProduction, setIsProduction] = useState<boolean>(false);
 
-  useMemo(() => {
+  useEffect(() => {
     if (window.location.hostname === 'handle.me') {
       setIsProduction(true);
     }
@@ -25,7 +25,7 @@ export const useMainDomain = (): string => {
   const [mainDomain, setMainDomain] = useState<string>('testnet.adahandle.me');
   const isProduction = useIsProduction();
 
-  useMemo(() => {
+  useEffect(() => {
     setMainDomain(isProduction ? 'adahandle.com' : 'testnet.adahandle.com');
   }, [isProduction]);
 
@@ -36,7 +36,7 @@ export const useCardanoscanDomain = (): string => {
   const [cardanoscanDomain, setCardanoscanDomain] = useState<string>('testnet.cardanoscan.io');
   const isProduction = useIsProduction();
 
-  useMemo(() => {
+  useEffect(() => {
     setCardanoscanDomain(isProduction ? 'cardanoscan.io' : 'testnet.cardanoscan.io');
   }, [isProduction]);
 
@@ -47,7 +47,7 @@ export const usePolicyID = (): string => {
   const [policyID, setPolicyID] = useState<string>('');
   const isProduction = useIsProduction();
 
-  useMemo(() => {
+  useEffect(() => {
     setPolicyID(isProduction ? 'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a' : '8d18d786e92776c824607fd8e193ec535c79dc61ea2405ddf3b09fe3');
   }, [isProduction]);
 
