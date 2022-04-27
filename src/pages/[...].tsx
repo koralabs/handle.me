8ebc221b9a969ec21ae582ff1dd2b17bda7249eb
guavajellyaaron <aaron@guavajellyco.com>
2022-04-26 17:25:38 -0700

    change lookup to hit api

--- src/pages/[...].tsx
+++ src/pages/[...].tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { LookupResponseBody } from "../../netlify/functions/location";
+import { LookupResponseBody } from "../../netlify/functions/lookupAddress";
 import SEO from "../components/seo";
 import "buffer";
 
@@ -27,6 +27,7 @@ function IndexPage({ params }) {
   const [validHandle, setValidHandle] = useState<boolean>(null);
   const [copying, setCopying] = useState<boolean>(false);
   const [isShellyAddress, setIsShellyAddress] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const mainDomain = useMainDomain();
   const policyID = usePolicyID();
@@ -57,7 +58,7 @@ function IndexPage({ params }) {
     const paths = route.split("/");
 
     const loadAddress = async () => {
-      await fetch("/.netlify/functions/location", {
+      await fetch("/.netlify/functions/lookupAddress", {
         headers: {
           [HEADER_HANDLE]: paths[0],
         },
@@ -66,6 +67,7 @@ function IndexPage({ params }) {
           const data: LookupResponseBody = await res.json();
           if (data.error) {
             setAddress(null);
+            setErrorMessage(data.message);
             return;
           }
 
@@ -78,6 +80,7 @@ function IndexPage({ params }) {
         })
         .catch((e) => {
           console.log(e);
+          setErrorMessage(e.message);
           setAddress(null);
         });
     };
@@ -91,6 +94,129 @@ function IndexPage({ params }) {
     setTimeout(() => {
       setCopying(false);
     }, 1000);
+  };
+
+  const renderDisplay = () => {
+    if (errorMessage) {
+      return (
+        <div>
+          <hr className="w-12 border-dark-300 border-2 block my-8" />
+          <div
+            style={{ backgroundColor: "#a94848" }}
+            className=" text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800 border-l-4 p-4"
+            role="error"
+          >
+            <p className="font-bold">Error</p>
+            <p>{errorMessage}</p>
+          </div>
+        </div>
+      );
+    }
+
+    if (address == null) {
+      return (
+        <>
+          <hr className="w-12 border-dark-300 border-2 block my-8" />
+          {message ? (
+            <h3>{message}</h3>
+          ) : (
+            <>
+              <h3>This Handle isn't Minted!</h3>
+              <Button
+                className="w-full mt-4"
+                href={`https://${mainDomain}/mint`}
+              >
+                Purchase Now &rarr;
+              </Button>
+            </>
+          )}
+        </>
+      );
+    }
+
+    return (
+      <>
+        <div className="relative">
+          <div
+            className={`focus:ring-0 focus:ring-opacity-0 border-2 focus:border-white outline-none form-input bg-dark-100 border-dark-300 rounded-lg px-6 py-4 text-lg text-dark-300 w-full overflow-hidden`}
+          >
+            {loading && "Loading..."}
+            {!loading && "string" === typeof address && (
+              <span style={{ transform: "translateX(-100%)" }}>
+                {address.substr(0, 10)}...
+                {address.substr(address.length - 5)}
+              </span>
+            )}
+            <button
+              onClick={handleCopy}
+              disabled={!address || !validHandle || loading}
+              className={`absolute top-0 right-0 h-full w-16 rounded-r-lg ${
+                copying ? "bg-primary-200" : "bg-primary-100"
+              }`}
+            >
+              <svg
+                className={`w-full height-full p-5 ${
+                  copying ? "hidden" : "block"
+                }`}
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fill="#fff"
+                  d="M18.783 13.198H15.73a.78.78 0 010-1.559h2.273V3.652H7.852v.922c0 .433-.349.78-.78.78a.778.778 0 01-.78-.78V2.872c0-.43.349-.78.78-.78h11.711c.431 0 .78.35.78.78v9.546a.781.781 0 01-.78.78z"
+                />
+                <path
+                  fill="#fff"
+                  d="M12.927 17.908H1.217a.781.781 0 01-.78-.78V7.581c0-.43.349-.78.78-.78h11.709c.431 0 .78.35.78.78v9.546c0 .43-.349.781-.779.781zm-10.93-1.56h10.15V8.361H1.997v7.987z"
+                />
+              </svg>
+
+              <svg
+                className={`w-full height-full p-5 ${
+                  copying ? "block" : "hidden"
+                }`}
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fill="#fff"
+                  d="M7.197 16.963h-.002a.773.773 0 01-.544-.227L.612 10.654a.769.769 0 011.09-1.084l5.495 5.536L18.221 4.083a.767.767 0 011.087 0c.301.3.301.787 0 1.087L7.741 16.738a.772.772 0 01-.544.225z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        {isShellyAddress ? (
+          <></>
+        ) : (
+          <div>
+            <hr className="w-12 border-dark-300 border-2 block my-8" />
+            <div
+              style={{ backgroundColor: "#bd8a45" }}
+              className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+              role="alert"
+            >
+              <p className="font-bold">Warning</p>
+              <p>
+                Address is does not appear to be a Shelly Wallet Address. If
+                address is a smart contract, it's possible that any ADA sent
+                might be lost.
+              </p>
+            </div>
+          </div>
+        )}
+        {fingerprintData && (
+          <p>
+            <a
+              target="_blank"
+              rel="noopener nofollow"
+              className="text-primary-100 mt-4 text-sm block"
+              href={`https://${cardanoscanDomain}/token/${policyID}.${fingerprintData.assetName}?tab=topholders`}
+            >
+              Verify on Cardanoscan &rarr;
+            </a>
+          </p>
+        )}
+      </>
+    );
   };
 
   return (
@@ -121,107 +247,7 @@ function IndexPage({ params }) {
                 {handle}
               </div>
             </div>
-            <div className="col-span-6 col-start-4 mb-2">
-              {null === address ? (
-                <>
-                  <hr className="w-12 border-dark-300 border-2 block my-8" />
-                  {message ? (
-                    <h3>{message}</h3>
-                  ) : (
-                    <>
-                      <h3>This Handle isn't Minted!</h3>
-                      <Button
-                        className="w-full mt-4"
-                        href={`https://${mainDomain}/mint`}
-                      >
-                        Purchase Now &rarr;
-                      </Button>
-                    </>
-                  )}
-                </>
-              ) : (
-                <>
-                  <div className="relative">
-                    <div
-                      className={`focus:ring-0 focus:ring-opacity-0 border-2 focus:border-white outline-none form-input bg-dark-100 border-dark-300 rounded-lg px-6 py-4 text-lg text-dark-300 w-full overflow-hidden`}
-                    >
-                      {loading && "Loading..."}
-                      {!loading && "string" === typeof address && (
-                        <span style={{ transform: "translateX(-100%)" }}>
-                          {address.substr(0, 10)}...
-                          {address.substr(address.length - 5)}
-                        </span>
-                      )}
-                      <button
-                        onClick={handleCopy}
-                        disabled={!address || !validHandle || loading}
-                        className={`absolute top-0 right-0 h-full w-16 rounded-r-lg ${
-                          copying ? "bg-primary-200" : "bg-primary-100"
-                        }`}
-                      >
-                        <svg
-                          className={`w-full height-full p-5 ${
-                            copying ? "hidden" : "block"
-                          }`}
-                          viewBox="0 0 20 20"
-                        >
-                          <path
-                            fill="#fff"
-                            d="M18.783 13.198H15.73a.78.78 0 010-1.559h2.273V3.652H7.852v.922c0 .433-.349.78-.78.78a.778.778 0 01-.78-.78V2.872c0-.43.349-.78.78-.78h11.711c.431 0 .78.35.78.78v9.546a.781.781 0 01-.78.78z"
-                          />
-                          <path
-                            fill="#fff"
-                            d="M12.927 17.908H1.217a.781.781 0 01-.78-.78V7.581c0-.43.349-.78.78-.78h11.709c.431 0 .78.35.78.78v9.546c0 .43-.349.781-.779.781zm-10.93-1.56h10.15V8.361H1.997v7.987z"
-                          />
-                        </svg>
-
-                        <svg
-                          className={`w-full height-full p-5 ${
-                            copying ? "block" : "hidden"
-                          }`}
-                          viewBox="0 0 20 20"
-                        >
-                          <path
-                            fill="#fff"
-                            d="M7.197 16.963h-.002a.773.773 0 01-.544-.227L.612 10.654a.769.769 0 011.09-1.084l5.495 5.536L18.221 4.083a.767.767 0 011.087 0c.301.3.301.787 0 1.087L7.741 16.738a.772.772 0 01-.544.225z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                  {isShellyAddress ? (
-                    <></>
-                  ) : (
-                    <div>
-                      <hr className="w-12 border-dark-300 border-2 block my-8" />
-                      <div
-                        style={{ backgroundColor: "#bd8a45" }}
-                        className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
-                        role="alert"
-                      >
-                        <p className="font-bold">Warning</p>
-                        <p>
-                          Address is does not appear to be a Shelly Wallet
-                          Address.
-                        </p>
-                      </div>
-                    </div>
-                  )}
-                  {fingerprintData && (
-                    <p>
-                      <a
-                        target="_blank"
-                        rel="noopener nofollow"
-                        className="text-primary-100 mt-4 text-sm block"
-                        href={`https://${cardanoscanDomain}/token/${policyID}.${fingerprintData.assetName}?tab=topholders`}
-                      >
-                        Verify on Cardanoscan &rarr;
-                      </a>
-                    </p>
-                  )}
-                </>
-              )}
-            </div>
+            <div className="col-span-6 col-start-4 mb-2">{renderDisplay()}</div>
           </div>
         </div>
       </section>
